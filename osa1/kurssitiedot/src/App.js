bf7654ba0cd9f108a0bca297e69b4eb02f0d2e16
Atte Marttinen <atte.i.marttinen@gmail.com>
2022-06-04 21:31:42 +0300

    App.js muokattu

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,9 +1,17 @@
-const Header = () => {
-
+const Header = (props) => {
+  return (
+    <>
+      <p>{props.course}</p>
+    </>
+  )
 }
 
-const Content = () => {
+const Content = (props) => {
+  return (
+    <>
 
+    </>
+  )
 }
 
 const Total = () => {
@@ -21,17 +29,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course="Half Stack application development" />
+      <Content part="Fundamentals of React" excercises=10 />      
+      <Total />
     </div>
   )
 }
