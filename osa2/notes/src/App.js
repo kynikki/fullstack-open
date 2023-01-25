b396b0fc0183b623e516743d0cd9afd8b277aecb
Atte Marttinen <atte.i.marttinen@gmail.com>
2023-01-25 18:51:23 +0200

    notesia työstetty

--- osa2/notes/src/App.js
+++ osa2/notes/src/App.js
@@ -3,10 +3,20 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...')
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-  const addNote = (event) => {    
-    console.log('button clicked', event.target)
+  const addNote = (event) => {
+    event.preventDefault()
+
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+
+     setNotes(notes.concat(noteObject))
+     setNewNote('')
   }
 
   const handleNoteChange = (event) => {
@@ -14,12 +24,21 @@ const App = (props) => {
     setNewNote(event.target.value)
 
   }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
   
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() =>setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map(note =>
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
