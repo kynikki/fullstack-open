0408fb69621426e5b329ba29dc93e49d0a088790
Atte Marttinen <atte.i.marttinen@gmail.com>
2023-01-30 18:57:44 +0200

    notes connected to json database

--- osa2/notes/src/App.js
+++ osa2/notes/src/App.js
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Note from './components/Note'
+import axios from 'axios'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])  
+  
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
