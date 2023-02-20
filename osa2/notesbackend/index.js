313ee78b1849794045fb437f504c0a57b8148d2b
Atte Marttinen <atte.i.marttinen@gmail.com>
2023-02-20 20:18:27 +0200

    started creating backend for notes

---
+++ osa2/notesbackend/index.js
@@ -0,0 +1,85 @@
+const { response } = require('express')
+
+const express = (require('express'))
+const app = express()
+
+// Add json-parser to the code
+app.use(express.json())
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)      
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        res.json(note)
+    }
+    else {
+        res.status(404).end()
+    }
+})
+
+app.delete('api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    res.status(202).end()
+})
+
+const generateId = () => {
+    const maxId = notes.lenght > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (req, res) =>{
+        
+    const body = req.body    
+    
+    if (!body.contet) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    }
+
+    notes = notes.concat(note)    
+    res.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+  
+
