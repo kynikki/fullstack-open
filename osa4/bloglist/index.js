d752d86b8c04f2a1b2e7b4fad3ef7f220c9e6683
Atte Marttinen <atte.i.marttinen@gmail.com>
2023-03-08 21:04:03 +0200

    bloglist started

---
+++ osa4/bloglist/index.js
@@ -0,0 +1,8 @@
+const app = require('./app')
+const config = require('./utils/config')
+const { info } = require('./utils/logger')
+
+
+app.listen(config.PORT, () => {
+  info(`Server running on port ${config.PORT}`)    
+})
